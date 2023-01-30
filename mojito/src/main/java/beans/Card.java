445d5c53d6e5bddd2516ca94baf4dd6e0419ec0e
damienduarte <damienduarte@free.fr>
2023-01-30 20:57:23 +0100

    commit

---
+++ mojito/src/main/java/beans/Card.java
@@ -0,0 +1,26 @@
+package beans;
+
+public class Card {
+	private Integer value;
+	private String color;
+	
+	public Card(Integer value, String color) {
+		
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
